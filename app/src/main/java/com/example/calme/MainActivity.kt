7f0eaae7ea02b094a58e2ef428973802710da13c
sabaghip <sabaghi.p@gmail.com>
2023-11-19 22:41:09 +0330

    feat : added state for navbar

--- app/src/main/java/com/example/calme/MainActivity.kt
+++ app/src/main/java/com/example/calme/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.calme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,8 +24,13 @@ import com.example.calme.TasksWindow.TasksWindow
 import com.example.calme.ui.theme.CalMeTheme
 
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.util.Date
 
+val taskWindow = TasksWindow()
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +38,20 @@ class MainActivity : ComponentActivity() {
             CalMeTheme {
                 val tasks = ArrayList<Task>()
                 initialize(tasks);
-                val taskWindow = TasksWindow()
+                var tabState by remember { mutableStateOf("tasks") }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        navBar("tasks")
-                        taskWindow.showTasks(tasks)
+                        navBar(tabState, {tabState = "tasks"}, {tabState = "ss1"}, {tabState = "ss2"}, {tabState = "ss3"})
+                        when(tabState){
+                            "tasks" -> taskWindow.showTasks(tasks)
+                            "ss1" -> test()
+                            else -> test()
+                        }
+
                     }
                 }
             }
@@ -48,21 +60,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun navBar(selected:String){
+fun navBar(selected:String, onClickTasks:() -> Unit, onClickss1:() -> Unit, onClickss2:() -> Unit, onClickss3:() -> Unit){
     Row(modifier= Modifier){
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="tasks") Color.Red else Color.Yellow))) {
+        Button(onClick = onClickTasks, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="tasks") Color.Red else Color.Yellow))) {
             Text(text = "Tasks")
         }
         Spacer(modifier = Modifier.width(10.dp))
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss1") Color.Red else Color.Yellow))) {
+        Button(onClick = onClickss1, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss1") Color.Red else Color.Yellow))) {
             Text(text = "ss1")
         }
         Spacer(modifier = Modifier.width(10.dp))
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss2") Color.Red else Color.Yellow))) {
+        Button(onClick = onClickss2, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss2") Color.Red else Color.Yellow))) {
             Text(text = "ss2")
         }
         Spacer(modifier = Modifier.width(10.dp))
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss3") Color.Red else Color.Yellow))) {
+        Button(onClick = onClickss3, colors = ButtonDefaults.outlinedButtonColors(containerColor=(if(selected=="ss3") Color.Red else Color.Yellow))) {
             Text(text = "ss3")
         }
     }
@@ -74,6 +86,10 @@ fun initialize(array: ArrayList<Task>){
     array.add(Task("Play Video Game", "play new Video Game which my friend bought for me.", Date(2023, 11, 19, 12, 23)))
 }
 
+@Composable
+fun test(){
+    Box(modifier= Modifier.background(Color.Green))
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
--- app/src/main/java/com/example/calme/TasksWindow/TasksWindow.kt
+++ app/src/main/java/com/example/calme/TasksWindow/TasksWindow.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,9 +22,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calme.Model.Task
+import com.example.calme.initialize
+import com.example.calme.navBar
+import com.example.calme.ui.theme.CalMeTheme
 
 class TasksWindow {
-
     @Composable
     fun showTasks(array : ArrayList<Task>) {
         LazyColumn(modifier= Modifier.padding(top=10.dp)){
