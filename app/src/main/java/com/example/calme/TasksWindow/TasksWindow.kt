e5a918ea8cadc097b66ea46b6bea6982db7363ff
sabaghip <sabaghi.p@gmail.com>
2023-11-20 21:31:19 +0330

    feat : text fields added to popUp

--- app/src/main/java/com/example/calme/TasksWindow/TasksWindow.kt
+++ app/src/main/java/com/example/calme/TasksWindow/TasksWindow.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,20 +18,26 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
@@ -42,6 +49,7 @@ import com.example.calme.ui.theme.CalMeTheme
 
 class TasksWindow {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun showTasks(array : ArrayList<Task>) {
         val popUpState = remember { mutableStateOf(false) }
@@ -62,11 +70,33 @@ class TasksWindow {
                     Box(
                         modifier = Modifier
                             .height(250.dp)
-                            .width(200.dp)
+                            .width(240.dp)
                             .background(Color(0xDDFF0000))
-                            .padding(start = 20.dp, top = 100.dp)
+                            .padding(start = 10.dp, top = 10.dp)
                     ) {
-                        Text(text = "Popup", fontSize = 30.sp)
+                        Column {
+                            var title by remember { mutableStateOf(TextFieldValue("")) }
+                            var description by remember { mutableStateOf(TextFieldValue("")) }
+                            TextField(
+                                value = title,
+                                onValueChange = { newValue -> title = newValue },
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .width(200.dp),
+                                label = { Text("Title") },
+                                placeholder = { Text("Title") },
+                            )
+                            TextField(
+                                value = description,
+                                onValueChange = { newValue -> description = newValue },
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .width(200.dp),
+                                label = { Text("Description") },
+                                placeholder = { Text("Description") },
+                            )
+                        }
+
                     }
                 }
             }
